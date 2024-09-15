7e2199d096b6d190d7a50a3e8aa18772ca3ed37f
David Hustace <david@opennms.org>
2024-09-15 13:33:21 -0400

    Spring SERVER OpenAPI generator of Client API

---
+++ client-case-api/src/test/java/com/beaconstrategists/com/client_ticket_api/ClientTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconstrategists.com.client_ticket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
