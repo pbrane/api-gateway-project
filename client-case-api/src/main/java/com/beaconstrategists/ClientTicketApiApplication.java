7a49f2d1b0febdcf77f29c36f870514c767f7e77
David Hustace <david@opennms.org>
2024-09-15 15:51:37 -0400

    cleaning up package names

--- client-case-api/src/main/java/com/beaconstrategists/ClientTicketApiApplication.java
+++ client-case-api/src/main/java/com/beaconstrategists/ClientTicketApiApplication.java
@@ -1,4 +1,4 @@
-package com.beaconstrategists.com.client_ticket_api;
+package com.beaconstrategists;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
